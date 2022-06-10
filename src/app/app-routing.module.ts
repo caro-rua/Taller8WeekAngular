f7659160bb6490414e79c1ee1a236441e978bbfd
carolina <carorua19@gmail.com>
2022-06-10 12:56:22 -0500

    Taller 8 Semana

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TemporadasComponent } from './componente/temporadas/temporadas.component';
+import { LibrosComponent } from './componente/libros/libros.component';
+import { JuegosComponent } from './componente/juegos/juegos.component';
+import { NoticiasComponent } from './componente/noticias/noticias.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'temporadas',component:TemporadasComponent},
+  {path:'libros',component:LibrosComponent},
+  {path:'juegos',component:JuegosComponent},
+  {path:'noticias',component:NoticiasComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraComponent } from './componente/barra/barra.component';
+import { InicioComponent } from './componente/inicio/inicio.component';
+import { FooterComponent } from './componente/footer/footer.component';
+import { TemporadasComponent } from './componente/temporadas/temporadas.component';
+import { LibrosComponent } from './componente/libros/libros.component';
+import { JuegosComponent } from './componente/juegos/juegos.component';
+import { NoticiasComponent } from './componente/noticias/noticias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent,
+    InicioComponent,
+    FooterComponent,
+    TemporadasComponent,
+    LibrosComponent,
+    JuegosComponent,
+    NoticiasComponent
   ],
   imports: [
     BrowserModule,
